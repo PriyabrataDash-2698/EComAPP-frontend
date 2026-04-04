35e3386e9929abe3b48c425a839bcba19a48f5ce
priyabratadash <pbd2698@gmail.com>
2026-04-04 15:10:08 +0530

    implemented_Product_reducer

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,11 @@
-const API_BASE_URL = "http://localhost:5454"
+const { default: axios } = require("axios");
+
+const API_BASE_URL = "http://localhost:5454";
+const jwt = localStorage.getItem("jwt");
+export const api = axios({
+    baseURL:API_BASE_URL,
+    headers:{
+        "Authorization":`Bearer ${jwt}`,
+        "Content-Type":"application/json"
+    }
+})
